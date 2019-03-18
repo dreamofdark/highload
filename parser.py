c3575e7bd50a0fd7c7543a8913958cf620fe19ee
dreamofdark <ksyunya.ku@gmail.com>
2019-03-18 12:39:47 +0300

    fix config

--- parser.py
+++ parser.py
@@ -2,29 +2,22 @@ def parse_conf():
 
     try:
         f = open('/etc/httpd.conf', 'r')
-        print('cfg opened')
     except FileNotFoundError:
-        print('cfg not found')
         exit('conf is not found')
 
     data = f.read()
     f.close()
 
-    print(data)
-
     if not data:
         exit('no data')
 
     conf = data.split('\n')
-    print('parse conf ', conf)
 
 
     result = {}
     for str in conf:
         pair = str.split(' ')
-        print(pair)
         if len(pair) < 2:
-            print('<2')
             continue
         result[pair[0]] = pair[1]
 
--- serv.py
+++ serv.py
@@ -6,8 +6,6 @@ from pool import Pool
 
 cpu, thread_number, root = parse_conf()
 
-print('CPU: %d\nthread_limit: %d\ndoc_root: %s\n' % (cpu, thread_number, root))
-
 sock = socket.socket()
 
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
